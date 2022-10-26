3566d0810b55b664c3e7c7236b557acbac0ff4dc
mlevental <maksim.levental@gmail.com>
2022-10-25 21:08:07 -0500

    update plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,8 +7,6 @@ import matplotlib.patheffects as PathEffects
 from matplotlib.container import BarContainer
 from matplotlib.lines import Line2D
 
-vitis_df = pd.read_csv("vitis_reports.csv", header=0, index_col=[0, 1, 2])
-bragghls_df = pd.read_csv("bragghls_reports.csv", header=0, index_col=[0, 1, 2])
 
 font = {
     # "family": "normal",
@@ -18,42 +16,59 @@ font = {
 matplotlib.rc("font", **font)
 
 
-# matplotlib.rcParams['text.usetex'] = True
-
-
 def myticks(x, pos):
     if x <= 0:
         return f"${x}$"
     exponent = int(np.log2(x))
-    coeff = x / 2 ** exponent
+    coeff = x / 2**exponent
     # return r"$2^{{ {:2d} }}$".format(exponent)
     return str(x)
 
 
-MAX_REGULAR = None
-BAR_CHART_COLORS = {}
-PATTERNS = {}
+def ticks_format(value, index):
+    """
+    get the value and returns the value as:
+       integer: [0,99]
+       1 digit float: [0.1, 0.99]
+       n*10^m: otherwise
+    To have all the number of the same size they are all returned as latex strings
+    """
+    if index % 2:
+        return ""
+
+    exp = np.floor(np.log10(value))
+    base = value / 10**exp
+    if exp == 0 or exp == 1:
+        return "${0:d}$".format(int(value))
+    if exp == -1:
+        return "${0:.1f}$".format(value)
+    else:
+        # return "${0:d}\\times 10^{{{1:d}}}$".format(int(base), int(exp))
+        # return "${0:d}e{{{1:d}}}$".format(int(base), int(exp))
+        return ""
+
+
+BAR_CHART_COLORS = {
+    "DSP": (0.12156862745098039, 0.4666666666666667, 0.7058823529411765, 1.0),
+    "BRAM": (1.0, 0.4980392156862745, 0.054901960784313725, 1.0),
+    "LUT": (0.17254901960784313, 0.6274509803921569, 0.17254901960784313, 1.0),
+    "FF": (0.8392156862745098, 0.15294117647058825, 0.1568627450980392, 1.0),
+}
+PATTERNS = {"DSP": "+", "BRAM": "x", "LUT": "o", "FF": "O"}
 
 
 def plot_metrics(sub_df, ax1, spacing=4, scale=10):
     i = 0
     patterns = ["/", "\\", "|", "-", "+", "x", "o", "O", ".", "*"]
     for metric in ["DSP", "BRAM", "LUT", "FF"]:
-        try:
-            vals = sub_df.loc[metric].sort_index()
-        except:
+        if metric not in dict(sub_df.index.values):
             continue
-        xs = np.array(vals.index.values)
-        bars = [i for i in ax1.containers if isinstance(i, BarContainer)]
 
-        global MAX_REGULAR
+        vals = sub_df.loc[metric].sort_index()
+        xs = np.array(vals.index.values)
+
         if xs[-1] == 1024:
-            MAX_REGULAR = xs[-2]
-            xs[-1] = MAX_REGULAR + spacing
-        if xs[-1] == 2048:
-            if MAX_REGULAR is None:
-                MAX_REGULAR = bars[1].patches[-1].xy[0] + 2 * spacing
-            xs[-1] = MAX_REGULAR + 2 * spacing
+            xs[-1] = xs[-2] + spacing
 
         vals = vals.values.flatten()
         if np.all(vals == 0):
@@ -62,193 +77,209 @@ def plot_metrics(sub_df, ax1, spacing=4, scale=10):
         if not len(diffs):
             diffs = [spacing / scale]
         diffs = np.append(diffs, [diffs[-1] * 1])
-        if metric in BAR_CHART_COLORS:
-            r = ax1.bar(
-                xs + diffs * (-1 + i),
-                vals,
-                width=diffs,
-                log=False,
-                ec="k",
-                color=BAR_CHART_COLORS[metric],
-                hatch=PATTERNS[metric],
-            )
-        else:
-            PATTERNS[metric] = patterns[i + 4]
-            r = ax1.bar(
-                xs + diffs * (-1 + i),
-                vals,
-                width=diffs,
-                log=False,
-                label=metric,
-                ec="k",
-                hatch=PATTERNS[metric],
-            )
-            BAR_CHART_COLORS[metric] = r.patches[0].get_facecolor()
+        ax1.bar(
+            xs + diffs * (-1 + i),
+            vals,
+            width=diffs,
+            log=False,
+            ec="k",
+            label=metric,
+            color=BAR_CHART_COLORS[metric],
+            hatch=PATTERNS[metric],
+        )
         i += 1
-        # ax1.plot(vals.index.values, vals.values, label=metric)
 
 
 def plot_unrolls():
+    vitis_df = pd.read_csv("vitis_reports.csv", header=0, index_col=[0, 1, 2])
+    bragghls_df = pd.read_csv("bragghls_reports.csv", header=0, index_col=[0, 1, 2])
     for i, mod in enumerate(vitis_df.index.levels[0]):
-        global MAX_REGULAR, BAR_CHART_COLORS, PATTERNS
-        MAX_REGULAR = None
-        BAR_CHART_COLORS = {}
-        PATTERNS = {}
-        fig, ax1 = plt.subplots(figsize=(10, 7))
+        fig, (lat_ax_vitis, resource_ax_bragghls) = plt.subplots(
+            1, 2, figsize=(10, 7), gridspec_kw={"width_ratios": [10, 1]}
+        )
+        resource_ax_vitis = lat_ax_vitis.twinx()
+        for ax in [
+            resource_ax_bragghls,
+            resource_ax_vitis,
+            lat_ax_vitis,
+        ]:
+            ax.set_yscale("log")
+
+        resource_ax_vitis.grid(which="both", axis="y", linestyle="--", zorder=-1)
+        resource_ax_vitis.set_axisbelow(True)
+        resource_ax_bragghls.grid(which="both", axis="y", linestyle="--", zorder=-1)
+        resource_ax_bragghls.set_axisbelow(True)
+
+        resource_ax_bragghls.set_xlim(2046, 2050)
 
         sub_df = vitis_df.loc[mod].swaplevel(0, 1)
-        plot_metrics(sub_df, ax1)
+        plot_metrics(sub_df, resource_ax_vitis)
         if mod in bragghls_df.index.levels[0]:
             bragghls_sub_df = bragghls_df.loc[mod].swaplevel(0, 1)
-            plot_metrics(bragghls_sub_df, ax1)
+            plot_metrics(bragghls_sub_df, resource_ax_bragghls)
 
-        # ax1.set_xscale("log")
-        ax1.set_yscale("log")
-        ax1.grid(which="major", axis="x", linestyle="--")
-        ax1.minorticks_off()  # turns off minor ticks
+        resource_ax_bragghls.set_ylabel("utilization (%)", fontdict={"fontsize": 25})
+        lat_ax_vitis.set_ylabel("time (μs)", fontdict={"fontsize": 25})
+        lat_ax_vitis.set_xlabel("unroll factor", fontdict={"fontsize": 25})
 
-        ax2 = ax1.twinx()
+        clock_vals = sub_df.loc["clock_period_minus_wns"].sort_index()
+        latency_vals = sub_df.loc["LatencyBest"].sort_index()
+        common_unroll_factors = sorted(
+            set(latency_vals.index.values) & set(clock_vals.index.values)
+        )
+        times = (
+            clock_vals.loc[common_unroll_factors].values
+            * latency_vals.loc[common_unroll_factors].values
+            / 1e3
+        ).flatten()
 
-        # if i % 2 == 0:
-        #     ax1.set_ylabel("utilization (%)", fontdict={"fontsize": 25})
-        # else:
-        #     ax2.set_ylabel("time (μs)", fontdict={"fontsize": 25})
-        ax1.set_ylabel("utilization (%)", fontdict={"fontsize": 25})
-        ax2.set_ylabel("time (μs)", fontdict={"fontsize": 25})
+        last_pos = [
+            common_unroll_factors[-2] + 4
+            if common_unroll_factors[-1] == 1024
+            else common_unroll_factors[-1]
+        ]
+        lat_ax_vitis.plot(
+            common_unroll_factors[:-1] + last_pos,
+            times,
+            # linestyle=":",
+            linewidth=2,
+            label="Vitis HLS",
+            color="magenta",
+            marker="x",
+        )
 
-        ax1.set_xlabel("unroll factor", fontdict={"fontsize": 25})
-
-        try:
-            clock_vals = sub_df.loc["clock_period_minus_wns"].sort_index()
-            latency_vals = sub_df.loc["LatencyBest"].sort_index()
-            common_unroll_factors = sorted(
-                set(latency_vals.index.values) & set(clock_vals.index.values)
-            )
-            times = (
-                clock_vals.loc[common_unroll_factors].values
-                * latency_vals.loc[common_unroll_factors].values
-                / 1e3
-            )
-            times = times.flatten()
-
-            if common_unroll_factors[-1] == 1024:
-                common_unroll_factors[-1] = common_unroll_factors[-2] + 4
-
-            ax2.plot(
-                common_unroll_factors,
-                times,
-                # linestyle=":",
-                linewidth=2,
-                label="Vitis HLS",
-                color="magenta",
-                marker="x",
-            )
-        except:
-            pass
-        
-        vitis_lat = times[-1]
-        vitis_va = "top"
-        if mod in bragghls_df.index.levels[0]:
-            bragghls_lat = bragghls_df.loc[mod, 2048, "LatencyBest"].item() * bragghls_df.loc[mod, 2048, "clock_period_minus_wns"].item() / 1e3
-            if vitis_lat < bragghls_lat:
-                vitis_va = "top"
-                bragghls_va = "bottom"
-            else:
-                vitis_va = "bottom"
-                bragghls_va = "top"
-
-        text = ax2.text(
-            common_unroll_factors[-1],
+        text = lat_ax_vitis.text(
+            last_pos[0],
             times[-1],
             f"{times[-1]:02f} μs",
             ha="right",
-            va=vitis_va,
+            va="bottom",
             color="magenta",
             weight="bold",
+            fontdict={"fontsize": 10},
         )
         text.set_path_effects([PathEffects.withStroke(linewidth=2, foreground="black")])
-        ax2.plot(
-            [common_unroll_factors[-1]],
+        lat_ax_vitis.plot(
+            last_pos,
             [times[-1]],
             marker="X",
             markersize=7,
             color="magenta",
-            markeredgecolor="k"
+            markeredgecolor="k",
         )
 
+        resource_ax_vitis.legend(
+            loc="upper left", title="Resources", framealpha=0.8, fontsize=20
+        ).set_zorder(102)
+        lat_ax_vitis.legend(
+            loc="upper right", title="Latency", framealpha=0.8, fontsize=20
+        ).set_zorder(102)
+        lat_legend = lat_ax_vitis.get_legend().legendHandles
+
         if mod in bragghls_df.index.levels[0]:
-            common_unroll_factors.append(48)
-            bragghls_lat = bragghls_df.loc[mod, 2048, "LatencyBest"].item() * bragghls_df.loc[mod, 2048, "clock_period_minus_wns"].item() / 1e3
-            text = ax2.text(
-                MAX_REGULAR + 2 * 4,
-                bragghls_lat,
+            red_patch = Line2D(
+                [0],
+                [0],
+                color="w",
+                label="BraggHLS",
+                markerfacecolor="r",
+                marker="X",
+                markeredgecolor="k",
+                markersize=15,
+            )
+            lat_ax_vitis.legend(
+                handles=lat_legend + [red_patch],
+                loc="upper right",
+                title="Latency",
+                framealpha=0.8,
+                fontsize=20,
+            )
+
+        lat_ax_vitis.xaxis.set_ticks(common_unroll_factors[:-1] + last_pos)
+        lat_ax_vitis.xaxis.set_ticklabels(common_unroll_factors)
+        resource_ax_bragghls.set_xticks([2048])
+        resource_ax_bragghls.set_xticklabels(["BraggHLS"])
+
+        resource_ax_vitis.tick_params(left=False, right=False)
+        resource_ax_vitis.yaxis.set_ticks([])
+        resource_ax_vitis.yaxis.set_ticklabels([])
+
+        resource_ax_bragghls.tick_params(left=False, right=False)
+        resource_ax_bragghls.yaxis.set_label_position("right")
+        resource_ax_bragghls.yaxis.tick_right()
+        resource_ax_bragghls.set_xticks([2048])
+        resource_ax_bragghls.set_xticklabels(["BraggHLS"])
+        resource_ax_bragghls.tick_params(axis="x", rotation=45)
+
+        lat_ax_vitis.tick_params(axis="x", rotation=45)
+        for ax in [
+            resource_ax_bragghls,
+            lat_ax_vitis,
+        ]:
+
+            ax.yaxis.set_major_formatter(ticks_format)  # remove the major ticks
+            ax.yaxis.set_minor_formatter(
+                ticker.FuncFormatter(ticks_format)
+            )  # add the custom ticks
+
+        max_ax, max_y = max(
+            [
+                (resource_ax_vitis, resource_ax_vitis.get_ylim()[1]),
+                (resource_ax_bragghls, resource_ax_bragghls.get_ylim()[1]),
+                (lat_ax_vitis, lat_ax_vitis.get_ylim()[1]),
+            ],
+            key=lambda x: x[1],
+        )
+        min_ax, min_y = min(
+            [
+                (resource_ax_vitis, resource_ax_vitis.get_ylim()[0]),
+                (resource_ax_bragghls, resource_ax_bragghls.get_ylim()[0]),
+                (lat_ax_vitis, lat_ax_vitis.get_ylim()[0]),
+            ],
+            key=lambda x: x[1],
+        )
+        # lat_ax_vitis.set_ylim(min_y, max_y)
+        resource_ax_vitis.set_ylim(min_y, max_y)
+        resource_ax_bragghls.set_ylim(min_y, max_y)
+
+        if mod in bragghls_df.index.levels[0]:
+            bragghls_lat = (
+                bragghls_df.loc[mod, 2048, "LatencyBest"].item()
+                * bragghls_df.loc[mod, 2048, "clock_period_minus_wns"].item()
+                / 1e3
+            )
+            text = resource_ax_bragghls.text(
+                2048,
+                min_y,
                 f"{bragghls_lat:02f} μs",
-                ha="right",
-                va=bragghls_va,
+                ha="center",
+                va="bottom",
                 color="red",
                 weight="bold",
+                fontdict={"fontsize": 10},
             )
-            text.set_path_effects([PathEffects.withStroke(linewidth=2, foreground="black")])
-            ax2.plot(
-                [MAX_REGULAR + 2 * 4],
-                [bragghls_lat],
+            text.set_path_effects(
+                [PathEffects.withStroke(linewidth=2, foreground="black")]
+            )
+            resource_ax_bragghls.plot(
+                [2048],
+                [min_y],
                 marker="X",
                 markersize=7,
                 color="red",
-                markeredgecolor="k"
+                markeredgecolor="k",
             )
 
-        # ax2.plot([0, 2048], [0.221, 0.221], marker='o', linestyle="--", label="BraggHLS", linewidth=2, color="red")
-        # text = ax2.text(
-        #     common_unroll_factors[-1], times[-1] + 1, f"{0.221} μs", ha="center", va="bottom", color="red", weight="bold"
-        # )
-        # text.set_path_effects([PathEffects.withStroke(linewidth=2, foreground='black')])
-        # # ax2.plot([common_unroll_factors[-1]], [0.221], marker='X', markersize=5, color="red")
-        # ax2.set_xlim([0, 1500])
-
-        ax2.grid(which="both", axis="y", linestyle="--")
-        # ax2.minorticks_off()  # turns off minor ticks
-
-        ax1.xaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-        ax2.xaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-
-        ax2.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-        ax1.set_xticks(common_unroll_factors)
-        labels = [item.get_text() for item in ax1.get_xticklabels()]
-        if common_unroll_factors[-1] == 48:
-            labels[-1] = "full"
-        if common_unroll_factors[-3] == 40:
-            labels[-2] = "1024"
-        ax1.set_xticklabels(labels)
-        ax2.set_xticklabels(labels)
-        ax1.tick_params(axis="x", rotation=45)
-
-        # if i == 1:
-        #     ax2.legend(loc="upper right", framealpha=1, fontsize=20).set_zorder(102)
-        # if i == 0:
-        #     ax1.legend(loc="upper left", title="Resources", framealpha=1, fontsize=20).set_zorder(102)
-        ax1.legend(
-            loc="upper left", title="Resources", framealpha=0.8, fontsize=20
-        ).set_zorder(102)
-        ax2.legend(
-            loc="upper right", title="Latency", framealpha=0.8, fontsize=20
-        ).set_zorder(102)
-        lat_legend = ax2.get_legend().legendHandles
-
-        if mod in bragghls_df.index.levels[0]:
-            red_patch = Line2D([0], [0], color='w', label='BraggHLS',
-                              markerfacecolor='r', marker="X", markeredgecolor="k",
-                    markersize=15)
-            ax2.legend(handles=lat_legend + [red_patch], loc="upper right", title="Latency", framealpha=0.8, fontsize=20)
-
-
+        # https://stackoverflow.com/a/59395256
+        lat_ax_vitis.set_zorder(resource_ax_vitis.get_zorder() + 1)
+        lat_ax_vitis.patch.set_visible(False)
         # https://github.com/matplotlib/matplotlib/issues/3706#issuecomment-817268918
         all_axes = fig.get_axes()
         for axis in all_axes:
             legend = axis.get_legend()
             if legend is not None:
                 legend.remove()
-                all_axes[-1].add_artist(legend)
+                lat_ax_vitis.add_artist(legend)
 
         fig.tight_layout()
         fig.savefig(f"{mod}.pdf")
@@ -259,9 +290,12 @@ def plot_unrolls():
     #     "font.family": "monospace",
     # })
 
+
+def plot_elapsed_times():
+    vitis_df = pd.read_csv("vitis_reports.csv", header=0, index_col=[0, 1, 2])
     fig, ax1 = plt.subplots(figsize=(10, 7))
     ax1.grid(which="major", linestyle="--")
-    ax1.minorticks_off()  # turns off minor ticks
+    # ax1.minorticks_off()  # turns off minor ticks
     elapsed_time_common = set()
 
     for i, mod in enumerate(vitis_df.index.levels[0]):
@@ -277,6 +311,8 @@ def plot_unrolls():
             xs,
             elapsed_time.values,
             # label=rf"$\mathtt{{{mod}}}$",
+            linewidth=2,
+            marker="x",
             label=f"{mod}",
         )
 
@@ -293,15 +329,19 @@ def plot_unrolls():
     fig.savefig(f"elapsed_time.pdf")
 
 
-# plot_unrolls()
-
 def plot_unroll_times():
     bragghls_df = pd.read_csv("unroll_conv.csv", header=0, index_col=[0])
     fig, ax1 = plt.subplots(figsize=(10, 7))
-    ax1.plot(bragghls_df.index.values, bragghls_df.values)
+    ax1.plot(
+        bragghls_df.index.values,
+        bragghls_df.values,
+        linewidth=2,
+        color="magenta",
+        marker="x",
+    )
     ax1.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
     ax1.grid(which="major", linestyle="--")
-    ax1.minorticks_off()  # turns off minor ticks
+    # ax1.minorticks_off()  # turns off minor ticks
     # ax1.set_xscale("log")
     # ax1.set_yscale("log")
     ax1.set_ylabel("time (s)", fontdict={"fontsize": 25})
@@ -313,5 +353,8 @@ def plot_unroll_times():
     fig.tight_layout()
     fig.savefig("conv_unroll.pdf")
 
-plot_unroll_times()
 
+if __name__ == "__main__":
+    plot_unrolls()
+    plot_unroll_times()
+    plot_elapsed_times()
